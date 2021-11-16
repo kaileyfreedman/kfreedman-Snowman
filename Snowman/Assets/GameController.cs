b7312515ae2521cd127f498c53bd4eecc0f2d9ab
kaileyfreedman <kfreedman2023@chadwickschool.org>
2021-11-16 10:38:42 -0800

    getting started

---
+++ Snowman/Assets/GameController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+   public UnityEngine.UI.Text Message;
+   public UnityEngine.UI.Button StartButton;
+
+}
