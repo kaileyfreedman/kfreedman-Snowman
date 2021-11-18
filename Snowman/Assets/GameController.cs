49d0d029975217d5e3c157ef4770550aee01ef7d
kaileyfreedman <kfreedman2023@chadwickschool.org>
2021-11-18 12:21:38 -0800

    challenge 1

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-   public UnityEngine.UI.Text Message;
-   public UnityEngine.UI.Button StartButton;
+    public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Button StartButton;
+    public GameObject StartScreen;
+    public GameObject PlayScreen;
 
+
+    public void StartGame()
+    {
+        this.StartScreen.SetActive(false);
+        this.PlayScreen.SetActive(true);
+    }
+
+    public void OpenStartScreen()
+    {
+        this.PlayScreen.SetActive(false);
+        this.StartScreen.SetActive(true);
+    }
+   
+   public void Start()
+   {
+        this.PlayScreen.SetActive(false);
+        this.StartScreen.SetActive(true);
+    }
 }
