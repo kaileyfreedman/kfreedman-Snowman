e8fbda78137341ebf914d07485c3bd6050d113f7
kaileyfreedman <kfreedman2023@chadwickschool.org>
2021-11-30 10:42:22 -0800

    worked on milestone 5

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -6,11 +6,25 @@ using WordGuesser;
 public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Text GetWord;
+    public UnityEngine.UI.Text GetGuessedLetters;
+    public UnityEngine.UI.Text GuessesRemaining;
+    public UnityEngine.UI.Text CheckGuess;
     public UnityEngine.UI.Button StartButton;
     public GameObject StartScreen;
     public GameObject PlayScreen;
     private WordGuesser.WordGame guessingGame;
+    public UnityEngine.UI.InputField PlayerGuess;
 
+    public void SubmitGuess()
+    {
+        string guess = PlayerGuess.text;
+        Debug.Log(guess);
+        GetWord.text = this.guessingGame.GetWord();
+        GetGuessedLetters.text = this.guessingGame.GetGuessedLetters();
+        CheckGuess.text = this.guessingGame.CheckGuess(PlayerGuess.text);
+        PlayerGuess.text = string.Empty;
+    }
 
     public void StartGame()
     {
